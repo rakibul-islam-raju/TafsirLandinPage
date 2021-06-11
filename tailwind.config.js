e1583d71b96a656eafa0f537dd882f909af43c57
Rakibul Islam <rakibul.islam7772588@gmail.com>
2021-06-11 22:05:46 +0600

    Landinpage initial design

---
+++ tailwind.config.js
@@ -0,0 +1,13 @@
+module.exports = {
+    purge: [],
+    darkMode: false, //false or 'media' or 'class'
+    theme: {
+      extend: {
+        fontFamily: {
+          body: ['Nunito']
+        }
+      },
+    },
+    variants: {},
+    plugins: [],
+  }
